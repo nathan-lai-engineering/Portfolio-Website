bcb3fd32a1ac1e98d9cb535ca8832c45257bd017
nathan <nlai274@gmail.com>
2023-12-17 21:22:09 -0800

    education page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
+import Education from "./pages/Education";
 import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/skills' element={<Skills />} />
+        <Route path='/education' element={<Education />} />
         <Route path='/experience' element={<Experience />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
---
+++ src/data/EducationData.ts
@@ -0,0 +1,15 @@
+interface EducationDataType {
+  title: string;
+  date: string;
+  details: string[];
+}
+
+export const EducationData: EducationDataType[] = [
+  {
+    title: 'Bachelor of Science, Computer Science',
+    date: ' August 2018 _ Jul 2023',
+    details: [
+      "CALIFORNIA STATE UNIVERSITY, LONG BEACH", 
+    ],
+  },
+];
